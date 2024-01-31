0a7d3b83fc9a7365dbc13609768848a23874b049
Dorahely Sanchez <dorahelys@gmail.com>
2024-01-30 19:57:56 -0600

    seeding data

--- server/models.py
+++ server/models.py
@@ -4,11 +4,12 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
-class User(db.model, SerializerMixin):
-    #table name
+# model #1 
+class User(db.Model, SerializerMixin):
+    # table name
     __tablename__ = "users"
     
-    #creating columns 
+    # creating columns 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     email = db.Column(db.String)
@@ -16,70 +17,72 @@ class User(db.model, SerializerMixin):
     position = db.Column(db.String)
     age = db.Column(db.Integer)
 
-    #creating ORM relationships - through the model aka class
-    #user = db.relationship('User', back_populates='users_emotions')
-    #users_emotions = db.relationship('User_Emotion', back_populates='user')
+    # creating ORM relationships - through the model aka class
+    # user = db.relationship('User', back_populates='users_emotions')
+    # users_emotions = db.relationship('User_Emotion', back_populates='user')
 
-    #user_like = db.relationship('User', back_populates='likes')
-    #likes = db.relationship('Like', back_populates='user_like')
+    # user_like = db.relationship('User', back_populates='likes')
+    # likes = db.relationship('Like', back_populates='user_like')
 
-    #user_journal = db.relationship('User', back_populates='journals')
-    #journals = db.relationship('Journal', back_populates='user_journal')
+    # user_journal = db.relationship('User', back_populates='journals')
+    # journals = db.relationship('Journal', back_populates='user_journal')
 
-    #serialization rules to stop recursion
+    # serialization rules to stop recursion
 
-class Emotion(db.model, SerializerMixin):
-    #table name
+#model #2
+class Emotion(db.Model, SerializerMixin):
+    # table name
     __tablename__ = "emotions"
 
-    #creating columns
+    # creating columns
     id = db.Column(db.Integer, primary_key=True)
     emotion = db.Column(db.String)
     date_stamp = db.Column(db.DateTime, server_default=db.func.now())
 
-    #creating ORM relationships - through the model aka class
-    #emotion = db.relationship('Emotion' , back_populates='users_emotions')
-    #users_emotions = db.relationship('User_Emotion', back_populates='emotion')
+    # creating ORM relationships - through the model aka class
+    # emotion = db.relationship('Emotion' , back_populates='users_emotions')
+    # users_emotions = db.relationship('User_Emotion', back_populates='emotion')
 
-    #emotion_like = db.relationship('Emotion' , back_populates='likes')
-    #likes = db.relationship('Like', back_populates='emotion_like')
+    # emotion_like = db.relationship('Emotion' , back_populates='likes')
+    # likes = db.relationship('Like', back_populates='emotion_like')
 
 
-class User_Emotion(db.model, SerializerMixin):
-    #table name
+class UserEmotion(db.Model, SerializerMixin):
+    # table name
     __tablename__ = "users_emotions"
 
-    #creating columns
+    # creating columns
     id = db.Column(db.Integer, primary_key=True)
 
-    #creating table relationships
+    # creating table relationships
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     emotion_id = db.Column(db.Integer, db.ForeignKey('emotions.id'))
 
-    #creating columns
+    # creating columns
     emotion_intensity = db.Column(db.Integer)
 
-    #creating ORM relationships - through the model aka class
-    #user = db.relationship('User', back_populates='users_emotions')
-    #emotion = db.relationship('Emotion' , back_populates='users_emotions')
+    # creating ORM relationships - through the model aka class
+    # user = db.relationship('User', back_populates='users_emotions')
+    # emotion = db.relationship('Emotion' , back_populates='users_emotions')
 
-class Like(db.model, SerializerMixin):
-    #table name
+
+class Like(db.Model, SerializerMixin):
+    # table name
     __tablename__ = "likes"
 
-    #creating columns
+    # creating columns
     id = db.Column(db.Integer, primary_key=True)
 
-    #creating table relationships
+    # creating table relationships
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     emotion_id = db.Column(db.Integer, db.ForeignKey('emotions.id'))
 
-    #creating ORM relationships - through the model aka class
-    #user_like = db.relationship('User', back_populates='likes')
-    #emotion_like = db.relationship('Emotion' , back_populates='likes')
+    # creating ORM relationships - through the model aka class
+    # user_like = db.relationship('User', back_populates='likes')
+    # emotion_like = db.relationship('Emotion' , back_populates='likes')
 
 '''
-class Journal(db.model, SerializerMixin):
+class Journal(db.Model, SerializerMixin):
     #table name
     __tablename__ = "journals"
 
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,54 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Emotion, UserEmotion, Like
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
-        print("Starting seed...")
+        
         # Seed code goes here!
+        # This will delete any existing rows
+        # so you can run the seed file multiple times without having duplicate entries in your database
+        print("Deleting data...")
+        User.query.delete()
+        Emotion.query.delete()
+        UserEmotion.query.delete()
+        Like.query.delete()
+        print("Starting seed...")
+
+        print("Creating users...")
+        user_1 = User(name="Dorahely", email='dorahely@gmail.com', password='passwords', position='engineer1', age=27)
+        user_2 = User(name="Monica", email='monica@gmail.com', password='passwords', position='engineer2', age=27)
+        user_3 = User(name="Kylie", email='kylie@gmail.com', password='passwords', position='engineer3', age=27)
+
+        db.session.add_all([user_1, user_2, user_3])
+        db.session.commit()
+
+        print("Creating emotions...")
+        emotion_1 = Emotion(emotion='anxious')
+        emotion_2 = Emotion(emotion='stressed')
+        emotion_3 = Emotion(emotion='happy')
+        emotion_4 = Emotion(emotion='happy')
+
+        db.session.add_all([emotion_1, emotion_2, emotion_3, emotion_4])
+        db.session.commit()
+
+        print("Creating join table...")
+        user_emotion_1 = UserEmotion(user_id=1, emotion_id=1, emotion_intensity=4)
+        user_emotion_2 = UserEmotion(user_id=2, emotion_id=2, emotion_intensity=3)
+        user_emotion_3 = UserEmotion(user_id=3, emotion_id=3, emotion_intensity=3)
+        user_emotion_4 = UserEmotion(user_id=1, emotion_id=4, emotion_intensity=4)
+
+        db.session.add_all([user_emotion_1, user_emotion_2, user_emotion_3, user_emotion_4])
+        db.session.commit()
+
+        print("Creating like table...")
+        like_1 = Like(user_id=1, emotion_id=2)
+        like_2 = Like(user_id=2, emotion_id=3)
+        like_3 = Like(user_id=3, emotion_id=2)
+
+        db.session.add_all([like_1, like_2, like_3])
+        db.session.commit()
+        
+        print("Seeding complete...")
