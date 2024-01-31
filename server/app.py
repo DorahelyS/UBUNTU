c6a237b1a828e26cbce62678549ec44ba3f88853
Dorahely Sanchez <dorahelys@gmail.com>
2024-01-31 15:50:57 -0600

    completed models and routes

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -11,11 +11,443 @@ from config import app, db, api
 # Add your model imports
 from models import db, User, Emotion, UserEmotion, Like
 
+
 # Views go here!
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+@app.route("/users", methods=['GET', 'POST'])
+def users():
+
+    #grabbing all users
+    all_users = User.query.all()
+
+    if all_users:
+        if request.method == 'GET':
+            response = make_response(
+                [user.to_dict()
+                 for user in all_users]
+            )
+            pass
+        elif request.method == 'POST':
+            try:
+                new_user_form = request.get_json()
+
+                new_user = User(
+                    new_user_name=new_user_form.get('name'),
+                    new_user_email=new_user_form.get('email'),
+                    new_user_password=new_user_form.get('password'),
+                    new_user_position=new_user_form.get('position'),
+                    new_user_age=new_user_form.get('age')
+                )
+
+                db.session.add(new_user)
+                db.session.commit()
+
+                response = make_response(
+                    new_user.to_dict(),
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot make new user'},
+                    500
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No users found in the DB"},
+            404
+        )
+
+    return response
+
+@app.route("/users/<int:id>", methods=['GET', 'PATCH', 'DELETE'])
+def users_by_id(id):
+
+    user_by_id = User.query.filter(
+        User.id == id).first()
+
+    if user_by_id:
+        if request.method == 'GET':
+            response = make_response(
+                user_by_id.to_dict(),
+                200
+            )
+
+        elif request.method == 'PATCH':
+            try:
+                update_user_form = request.get_json()
+
+                for attribute in update_user_form:
+                    setattr(user_by_id, attribute,
+                            update_user_form[attribute])
+
+                db.session.add(user_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    user_by_id.to_dict(),
+                    202
+                )
+            except:
+                response = make_response(
+                    {'error': 'Cannot update user'},
+                    405
+                )
+
+        elif request.method == 'DELETE':
+            try:
+                assoc_assets = UserEmotion.query.filter(
+                    UserEmotion.user_id == id) | Like.query.filter(Like.user_id == id).all()
+
+                for del_asset in assoc_assets:
+                    db.session.delete(del_asset)
+
+                db.session.delete(user_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    {'success': "Successfully deleted user"},
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot delete user'},
+                    405
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No user with that id"},
+            404
+        )
+
+    return response
+
+@app.route("/emotions", methods=['GET', 'POST'])
+def emotions():
+
+    all_emotions = Emotion.query.all()
+
+    if all_emotions:
+        if request.method == 'GET':
+            response = make_response(
+                [emotion.to_dict()
+                 for emotion in all_emotions]
+            )
+            pass
+        elif request.method == 'POST':
+            try:
+                new_emotion_form = request.get_json()
+
+                new_emotion = Emotion(
+                    new_emotion=new_emotion_form.get('emotion')
+                )
+
+                db.session.add(new_emotion)
+                db.session.commit()
+
+                response = make_response(
+                    new_emotion.to_dict(),
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot make new emotion'},
+                    500
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No emotion found in the DB"},
+            404
+        )
+
+    return response
+
+@app.route("/emotions/<int:id>", methods=['GET', 'PATCH', 'DELETE'])
+def emotions_by_id(id):
+
+    emotion_by_id = Emotion.query.filter(
+        Emotion.id == id).first()
+
+    if emotion_by_id:
+        if request.method == 'GET':
+            response = make_response(
+                emotion_by_id.to_dict(),
+                200
+            )
+
+        elif request.method == 'PATCH':
+            try:
+                update_emotion_form = request.get_json()
+
+                for attribute in update_emotion_form:
+                    setattr(emotion_by_id, attribute,
+                            update_emotion_form[attribute])
+
+                db.session.add(emotion_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    emotion_by_id.to_dict(),
+                    202
+                )
+            except:
+                response = make_response(
+                    {'error': 'Cannot update emotion'},
+                    405
+                )
+
+        elif request.method == 'DELETE':
+            try:
+                assoc_assets = UserEmotion.query.filter(
+                    UserEmotion.emotion_id == id) | Like.query.filter(Like.emotion_id == id).all()
+
+                for del_asset in assoc_assets:
+                    db.session.delete(del_asset)
+
+                db.session.delete(emotion_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    {'success': "Successfully deleted records"},
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot delete emotion'},
+                    405
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No emotion with that id"},
+            404
+        )
+
+    return response
+
+
+@app.route("/likes", methods=['GET', 'POST'])
+def likes():
+
+    all_likes = Like.query.all()
+
+    if all_likes:
+        if request.method == 'GET':
+            response = make_response(
+                [like.to_dict()
+                 for like in all_likes]
+            )
+            pass
+        elif request.method == 'POST':
+            try:
+                new_like_form = request.get_json()
+
+                new_like = Like(
+                    new_like_user_id=new_like_form.get('user_id'),
+                    new_like_emotion_id=new_like_form.get('emotion_id')
+                )
+
+                db.session.add(new_like)
+                db.session.commit()
+
+                response = make_response(
+                    new_like.to_dict(),
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot make new like'},
+                    500
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No likes in the DB"},
+            404
+        )
+
+    return response
+
+@app.route("/likes/<int:id>", methods=['GET', 'PATCH', 'DELETE'])
+def likes_by_id(id):
+
+    like_by_id = Like.query.filter(
+        Like.id == id).first()
+
+    if like_by_id:
+        if request.method == 'GET':
+            response = make_response(
+                like_by_id.to_dict(),
+                200
+            )
+
+        elif request.method == 'PATCH':
+            try:
+                update_like_form = request.get_json()
+
+                for attribute in update_like_form:
+                    setattr(like_by_id, attribute,
+                            update_like_form[attribute])
+
+                db.session.add(like_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    like_by_id.to_dict(),
+                    202
+                )
+            except:
+                response = make_response(
+                    {'error': 'Cannot update like'},
+                    405
+                )
+
+        elif request.method == 'DELETE':
+            try:
+
+                db.session.delete(like_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    {'success': "Successfully deleted like form records"},
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot delete like'},
+                    405
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No like with that id"},
+            404
+        )
+
+    return response
+
+@app.route("/user_emotion", methods=['GET', 'POST'])
+def user_emotion():
+
+    all_user_emotion = UserEmotion.query.all()
+
+    if all_user_emotion:
+        if request.method == 'GET':
+            response = make_response(
+                [user_emotion.to_dict()
+                 for user_emotion in all_user_emotion]
+            )
+            pass
+        elif request.method == 'POST':
+            try:
+                new_user_emotion_form = request.get_json()
+
+                new_user_emotion = UserEmotion(
+                    user_id=new_user_emotion_form.get('user_id'),
+                    emotion_id=new_user_emotion_form.get('emotion_id'),
+                    emotion_intensity=new_user_emotion_form('emotion_intensity')
+                )
+
+                db.session.add(new_user_emotion)
+                db.session.commit()
+
+                response = make_response(
+                    new_user_emotion.to_dict(),
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot make'},
+                    500
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "No found in the DB"},
+            404
+        )
+
+    return response
+
+@app.route("/user_emotion/<int:id>", methods=['GET', 'PATCH', 'DELETE'])
+def user_emotion_by_id(id):
+
+    user_emotion_by_id = UserEmotion.query.filter(
+        UserEmotion.id == id).first()
+
+    if user_emotion_by_id:
+        if request.method == 'GET':
+            response = make_response(
+                user_emotion_by_id.to_dict(),
+                200
+            )
+
+        elif request.method == 'PATCH':
+            try:
+                update_form = request.get_json()
+
+                for attribute in update_form:
+                    setattr(user_emotion_by_id, attribute,
+                            update_form[attribute])
+
+                db.session.add(user_emotion_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    user_emotion_by_id.to_dict(),
+                    202
+                )
+            except:
+                response = make_response(
+                    {'error': 'Cannot update'},
+                    405
+                )
+
+        elif request.method == 'DELETE':
+            try:
+
+                db.session.delete(user_emotion_by_id)
+                db.session.commit()
+
+                response = make_response(
+                    {'success': "Successfully deleted records"},
+                    201
+                )
+
+            except:
+                response = make_response(
+                    {'error': 'Cannot delete'},
+                    405
+                )
+
+            pass
+    else:
+        response = make_response(
+            {'error': "Nothing with that id"},
+            404
+        )
+
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
 # Models go here!
-# model #1 
+# independet model #1 
 class User(db.Model, SerializerMixin):
     # table name
     __tablename__ = "users"
@@ -19,17 +20,19 @@ class User(db.Model, SerializerMixin):
 
     # creating ORM relationships - through the model aka class
     # user = db.relationship('User', back_populates='users_emotions')
-    # users_emotions = db.relationship('User_Emotion', back_populates='user')
+    users_emotions = db.relationship('UserEmotion', back_populates='user')
 
-    # user_like = db.relationship('User', back_populates='likes')
-    # likes = db.relationship('Like', back_populates='user_like')
+    # user_like = db.relationship('User', back_populates='users_likes')
+    users_likes = db.relationship('Like', back_populates='user_like')
 
     # user_journal = db.relationship('User', back_populates='journals')
     # journals = db.relationship('Journal', back_populates='user_journal')
 
     # serialization rules to stop recursion
+    serialize_rules = ('-users_emotions.user', '-users_emotions.emotion', '-users_likes.emotion_like','-users_likes.user_like')
 
-#model #2
+
+# independent model #2
 class Emotion(db.Model, SerializerMixin):
     # table name
     __tablename__ = "emotions"
@@ -40,16 +43,20 @@ class Emotion(db.Model, SerializerMixin):
     date_stamp = db.Column(db.DateTime, server_default=db.func.now())
 
     # creating ORM relationships - through the model aka class
-    # emotion = db.relationship('Emotion' , back_populates='users_emotions')
-    # users_emotions = db.relationship('User_Emotion', back_populates='emotion')
+    # emotion = db.relationship('Emotion' , back_populates='emotions_users')
+    emotions_users = db.relationship('UserEmotion', back_populates='emotion')
 
-    # emotion_like = db.relationship('Emotion' , back_populates='likes')
-    # likes = db.relationship('Like', back_populates='emotion_like')
+    # emotion_like = db.relationship('Emotion' , back_populates='emotions_likes')
+    emotions_likes = db.relationship('Like', back_populates='emotion_like')
+
+    # serialization rules to stop recursion
+    serialize_rules = ('-emotions_users.emotion', '-emotions_users.user','-emotions_likes.user_like','-emotions_likes.emotion_like')
 
 
+# independent model #3
 class UserEmotion(db.Model, SerializerMixin):
     # table name
-    __tablename__ = "users_emotions"
+    __tablename__ = "user_emotion"
 
     # creating columns
     id = db.Column(db.Integer, primary_key=True)
@@ -62,10 +69,13 @@ class UserEmotion(db.Model, SerializerMixin):
     emotion_intensity = db.Column(db.Integer)
 
     # creating ORM relationships - through the model aka class
-    # user = db.relationship('User', back_populates='users_emotions')
-    # emotion = db.relationship('Emotion' , back_populates='users_emotions')
+    user = db.relationship('User', back_populates='users_emotions')
+    emotion = db.relationship('Emotion' , back_populates='emotions_users')
 
+    # serialization rules to stop recursion
+    serialize_rules = ('-user.users_emotions', '-emotion.emotions_users')
 
+# independent model #4
 class Like(db.Model, SerializerMixin):
     # table name
     __tablename__ = "likes"
@@ -78,10 +88,14 @@ class Like(db.Model, SerializerMixin):
     emotion_id = db.Column(db.Integer, db.ForeignKey('emotions.id'))
 
     # creating ORM relationships - through the model aka class
-    # user_like = db.relationship('User', back_populates='likes')
-    # emotion_like = db.relationship('Emotion' , back_populates='likes')
+    user_like = db.relationship('User', back_populates='users_likes')
+    emotion_like = db.relationship('Emotion' , back_populates='emotions_likes')
+
+    # serialization rules to stop recursion
+    serialize_rules = ('-user_like.users_likes', '-emotion_like.emotions_likes')
 
 '''
+# independent model #5 - STRETCH GOAL
 class Journal(db.Model, SerializerMixin):
     #table name
     __tablename__ = "journals"
